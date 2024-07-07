0ad8a3033659945031c519bce200cdb939f95139
VuMaiQuan <vumaiquan28062002@gmail.com>
2024-07-07 21:45:20 +0700

    len

--- template/src/admin/data/interface/user.ts
+++ template/src/admin/data/interface/user.ts
@@ -1,4 +1,30 @@
 export interface UserLogin {
+  id: string;
   email: string;
   password: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
 }
+export interface LoginData {
+  email: string;
+  password: string;
+}  
+
+const LoginUser = async (UserLogin: LoginData) => {
+  try {
+    const response = await fetch('https://reqres.in/api/login', {
+      method: "Post",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(UserLogin)
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export {LoginUser}
--- template/src/main.tsx
+++ template/src/main.tsx
@@ -6,6 +6,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from 'admin/routes/router';
 import { theme } from 'admin/theme/theme';
 import './index.css';
+import  'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
