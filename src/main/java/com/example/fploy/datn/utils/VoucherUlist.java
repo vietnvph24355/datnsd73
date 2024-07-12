00a6f09eb00823531935fa94c4a51053f5dacc1b
Con_Sen <90837200+vietnvph24355@users.noreply.github.com>
2024-07-12 20:30:06 +0700

    không động vào voucher

--- src/main/java/com/example/fploy/datn/utils/VoucherUlist.java
+++ src/main/java/com/example/fploy/datn/utils/VoucherUlist.java
@@ -6,31 +6,31 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.sql.Date;
+import java.util.Date;
 
 @Component
 @Primary
 public class VoucherUlist {
 
-    public TrangThaiVoucher setTrangThaiVoucher(Date ngayBatDau, Date ngayKetThuc) {
-        // Kiểm tra xem voucher đã bị hủy chưa
-
-        LocalDateTime currentDate = LocalDateTime.now();
-        LocalDate currentDateOnly = currentDate.toLocalDate();
-
-        LocalDate batDau = ngayBatDau.toLocalDate();
-        LocalDate ketThuc = ngayKetThuc.toLocalDate();
-
-        if (currentDateOnly.isBefore(batDau)) {
-            return TrangThaiVoucher.UPCOMING;
-        } else if (currentDateOnly.isEqual(batDau) || (currentDateOnly.isAfter(batDau) && currentDateOnly.isBefore(ketThuc))) {
-            return TrangThaiVoucher.ONGOING;
-        } else if (currentDateOnly.isBefore(ketThuc)) {
-            return TrangThaiVoucher.ENDING_SOON;
-        } else if (currentDateOnly.isEqual(ketThuc) || currentDateOnly.isAfter(ketThuc)) {
-            return TrangThaiVoucher.EXPIRED;
-        } else {
-            return TrangThaiVoucher.OUT_OF_STOCK;
-        }
-    }
+//    public TrangThaiVoucher setTrangThaiVoucher(Date ngayBatDau, Date ngayKetThuc) {
+//        // Kiểm tra xem voucher đã bị hủy chưa
+//
+//        LocalDateTime currentDate = LocalDateTime.now();
+//        LocalDate currentDateOnly = currentDate.toLocalDate();
+//
+//        LocalDate batDau = ngayBatDau.toLocalDate();
+//        LocalDate ketThuc = ngayKetThuc.toLocalDate();
+//
+//        if (currentDateOnly.isBefore(batDau)) {
+//            return TrangThaiVoucher.UPCOMING;
+//        } else if (currentDateOnly.isEqual(batDau) || (currentDateOnly.isAfter(batDau) && currentDateOnly.isBefore(ketThuc))) {
+//            return TrangThaiVoucher.ONGOING;
+//        } else if (currentDateOnly.isBefore(ketThuc)) {
+//            return TrangThaiVoucher.ENDING_SOON;
+//        } else if (currentDateOnly.isEqual(ketThuc) || currentDateOnly.isAfter(ketThuc)) {
+//            return TrangThaiVoucher.EXPIRED;
+//        } else {
+//            return TrangThaiVoucher.OUT_OF_STOCK;
+//        }
+//    }
 }
